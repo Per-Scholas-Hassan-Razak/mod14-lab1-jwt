96ea1cb5f04f5291f43a69469fd537c00c898803
hassanrazak <76017055+hassanrazak@users.noreply.github.com>
2025-08-27 11:12:36 -0400

    verify route added

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const {registerNewUser,loginUser} = require('../controllers/authController')
+const { verifyToken } = require('../utils/jwt')
 
 router.post("/register", registerNewUser)
 router.post("/login", loginUser)
+router.get("/verify", verifyToken,(req, res) => {
+  res.json({
+    message: "Token is valid. User authenticated.",
+    user: req.user,
+  })
+}
+)
 
 
 module.exports = router
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -6,8 +6,19 @@ const signToken = (payload) => {
     return jwt.sign(payload, secret, {expiresIn})
 }
 
-const verifyToken = (token) => {
-    return jwt.verify(token,secret)
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    if(!authHeader || !authHeader.startsWith("Bearer ")){
+        res.status(401).json({error:"No token provide or invalid login"})
+    }
+    const token = authHeader.split(" ")[1]
+      try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; 
+    next();
+  } catch (err) {
+    return res.status(403).json({ error: "Token is invalid or expired" });
+  }
 }
 
 const decodeToken = (token) => {
